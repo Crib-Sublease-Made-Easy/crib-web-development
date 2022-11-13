6dada1bee6514ae892b249de07cad0a648f6a034
Hadeid <hadeidmirza04@gmail.com>
2022-11-13 17:27:40 -0600

    Create first version of filter screen

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import MessagePage from './pages/Message';
+import FilterScreen1 from './pages/Filter';
 
 function App() {
   return (
     <div>
+      <FilterScreen1/>
       <MessagePage/>
     </div>
   )
---
+++ src/pages/Filter.js
@@ -0,0 +1,20 @@
+import filter from '../assets/filterScreen.svg';
+
+function FilterScreen1() {
+  return (
+    <div className='ml-6 flex flex-col lg:flex-row lg:p-20'>
+      <img className='pl-4 md:pt-20 lg:pl-60 md:pb-20' src={filter}>
+      </img>
+      <div className='w-50 p-4 lg:pt-60 lg:pl-60 lg:pr-40'>
+        <h1 className='border-box w-90 text-left'>
+          <p className='font-oswald font-bold text-6xl'> <span className='text-[#8559E3]'> Filtered</span> to your needs</p>
+        </h1>
+        <p className='font-roboto p-0 lg:pt-10 lg:pr-10 text-left text-3xl text-soft'>
+          Filter search results to find the perfect sublease for you! we offer more than 10+ filters to find your desired <div className='inline text-[#8559E3] font-semibold'>crib</div>
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default FilterScreen1;
