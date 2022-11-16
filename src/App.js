28676d3da77560e5112336e33a82ddd0ca2a9386
redrossa <adriano.raksi@gmail.com>
2022-11-15 18:14:29 -0600

    Implement features page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import MessagePage from './pages/Message';
 import FilterScreen from './pages/Filter';
 import MapScreen from './pages/Map';
 import Landing from "./pages/Landing";
+import Features from "./pages/Features";
 
 function App() {
   return (
     <div>
       <Landing />
+      <Features />
       <MapScreen/>
       <FilterScreen/>
       <MessagePage/>
---
+++ src/pages/Features.js
@@ -0,0 +1,52 @@
+import MapLocation from '../assets/map-location.png';
+import Settings from '../assets/settings.png';
+import MessageDetail from '../assets/message-detail.png';
+
+
+const Features = () => {
+  const features = [
+    {
+      icon: MapLocation,
+      title: 'Find',
+      text: 'Find subleases by location or on map'
+    },
+    {
+      icon: Settings,
+      title: 'Filter',
+      text: 'Filter subleases to find the one that fits'
+    },
+    {
+      icon: MessageDetail,
+      title: 'Connect',
+      text: 'Connect to tenants and ask more about it'
+    }
+  ]
+
+  return (
+      <div className="bg-white flex flex-col justify-center items-center md:px-80 mt-80">
+        <h1 className="font-oswald font-bold text-6xl">
+          Finding a sublease should be easy
+        </h1>
+        <h4 className="font-roboto pt-10 pr-10 text-2xl text-brand">
+          We are here to make your life easier!
+        </h4>
+        <div className="grid grid-cols-3 w-full justify-items-center gap-32 mt-16">
+          {features.map(feature => (
+              <div className="flex flex-col items-center gap-8">
+                <button className={`
+                  w-24 h-24 bg-white 
+                  flex items-center justify-center 
+                  rounded-lg shadow-brand shadow-2xl hover:shadow-brand hover:shadow-xl
+                `}>
+                  <img src={feature.icon} alt={feature.title} />
+                </button>
+                <h5 className="font-roboto text-3xl font-bold w-full text-center mt-8">{feature.title}</h5>
+                <p className="font-roboto text-2xl w-full text-center">{feature.text}</p>
+              </div>
+          ))}
+        </div>
+      </div>
+  );
+}
+
+export default Features;
