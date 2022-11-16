dafa0effb4452cd1ae9dacc8905277edb066eceb
Hadeid Mirza <hadeidmirza04@gmail.com>
2022-11-16 12:52:07 -0600

    modify map page to properly resize on a max screen

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,28 +1,29 @@
 import map from '../assets/map.svg';
 import search from '../assets/search-48.png'
 
+
 function MapScreen() {
   return (
-    <div className='bg-white flex flex-col lg:flex-row md:m-80'>
+    <div className='bg-white flex flex-row gap-6 md:mx-60 mt-20'>
     {/* The color is  #8559E3 purple, black, white*/}
-    <div className='w-50 p-10'>
-      <div className='bg-white drop-shadow-xl hover:drop-shadow-md rounded-xl max-w-md p-4'>
-        <img alt='searchIcon' className='inline h-10 pr-3 hover:animate-wiggle' src={search}>
+    <div className='w-96 p-0'>
+      <div className='border-box w-80 h-15 bg-white drop-shadow-xl rounded-lg p-4'>
+        <img alt='searchIcon' className='inline h-9 pr-3 hover:animate-wiggle' src={search}>
         </img>
-        <h1 className='text-xl inline'>
+        <h1 className='text-xl  inline'>
           Enter your location...
           </h1>
       </div>
-      <div className='mt-20 pt-6 '>
-        <h1 className='font-oswald font-bold text-6xl'>
+      <div className='mt-20 pt-2 '>
+        <h1 className='font-oswald font-bold text-5xl'>
           Search by <span className='text-[#8559E3]'>location</span> or look through the <span className='text-[#8559E3]'>map</span>
         </h1>
-      <p className='font-roboto pt-10 pr-10 text-left text-3xl'>
+      <p className='font-roboto pt-10 pr-10 text-left text-2xl'>
         Enter your location and find subleases through the search bar or move around the map to find subleases around the area you're looking at
       </p>
       </div>
     </div>
-    <img alt='mapIcon' src={map} className='pl-20 rounded-lg'>
+    <img alt='mapIcon' src={map} className='ml-20 pl-20 rounded-lg'>
     </img>
   </div>
   )
